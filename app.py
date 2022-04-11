86b494e4556020d9321f5d407f7501e2debfd6ab
Your Name <you@example.com>
2022-04-11 18:24:14 +0200

    second commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Response, request, Flask, jsonify
 from flask_cors import CORS
 # from Views.view import main
 # from Models.model import db
-# from werkzeug.serving import WSGIRequestHandler
+from werkzeug.serving import WSGIRequestHandler
 
 app = Flask(__name__)
 
