4a78a835b0068f1b3ff7e64504cb7afe78c0584c
Your Name <you@example.com>
2022-04-11 18:34:08 +0200

    second commit

---
+++ s.py
@@ -0,0 +1,15 @@
+
+from flask import Response, request, Flask, jsonify
+from flask_cors import CORS
+from Views.view import main
+from Models.model import db
+from werkzeug.serving import WSGIRequestHandler
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({"messae":"aaaaaa"})
+
+if __name__ == '__main__':
+    app.run()
